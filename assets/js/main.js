9b88be5af0f50a2f167cb25de7b9cd58ce87aec2
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2023-06-24 16:57:58 +0000

    responsivite sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const trendingPage = document.querySelector('.trending')
 // console.log(sidebarLists);
 sidebarLists.forEach((list) => {
   list.addEventListener('click', function () {
-    console.log(this);
+    // console.log(this);
     // console.log(this.classList.contains('active'));
     // console.log(this.children[0].classList.contains('active'));
     if (this.children[0].classList.contains('active')) {
@@ -34,18 +34,18 @@ sidebarLists.forEach((list) => {
 videos.forEach(video => {
   video.addEventListener('mouseenter', () => {
     const videoParent = video.parentElement.parentElement
-    console.log(avatar.style.width)
+    // console.log(avatar.style.width)
     videoParent.style.height = '165px'
     avatarSpan.style.bottom = '2px'
     avatar.style.width = '40px'
     avatar.style.height = '40px'
     avatarIcon.style.fontSize = "12px"
-    console.log(avatarIcon.style);
+    // console.log(avatarIcon.style);
     video.autoplay = true
     video.load()
   })
   video.addEventListener('mouseleave', () => {
-    console.log('leave');
+    // console.log('leave');
     const videoParent = video.parentElement.parentElement
     videoParent.style.height = '120px'
     avatarSpan.style.bottom = '5px'
